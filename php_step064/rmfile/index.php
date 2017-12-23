f4682254885592d46c8829cc83a28a79aeecae69
slavicc064 <slavicc064@gmail.com>
2017-12-24 01:17:55 +0200

    23.12.17 23:45

--- php_step064/rmfile/index.php
+++ php_step064/rmfile/index.php
@@ -1,51 +1,88 @@
 <style>
+    .test {
+        width: 90%;
+        height: auto;
+        margin-left: auto;
+        margin-right: auto;
+    }
 	.test div {
-		width: 25%;
-		heught: auto;
-		padding: 5px;
+		width: 390px;
+		height: 390px;
+		margin: 5px;
 		display: block;
 		float: left;
 		position: relative;
 	}
+    .test .test2 {
+        width: 95%;
+        height: 95%;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid gray;
+        border-radius: 4px;
+        transition: all 0.5s;
+    }
+    .test .test2:hover {
+        width: 450px;
+        height: 450px;
+        z-index: 100;
+        position: relative;
+        margin-left: -30px;
+        margin-top: -30px;
+        transition: all 0.5s;
+    }
+    .test div:hover span {
+        z-index: 101;
+    }
 	img {
 		width: 100%;
-	}
-	.test {
-		width: 100%;
+        height: 100%;
 	}
 	.test div span {
-		width: 30px;
-		height: 30px;
-		position: absolute;
-		top: 10px;
-		right: 10px;
-		background-color: green;
-		color: white;
-		font-size: 24px;
-		text-align: center;
-		border-radius: 50%; 
-		font-weight: bold;
-		cursor: pointer;
+        width: 30px;
+        height: 30px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        background-color: green;
+        color: white;
+        font-size: 36px;
+        text-align: center;
+        border-radius: 50%;
+        font-weight: bold;
+        line-height: 30px;
+        cursor: pointer;
+        -webkit-transform: rotate(45deg);
+        -moz-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        -o-transform: rotate(45deg);
+        transform: rotate(45deg);
+        opacity: 0.7;
 	}
     .test div span:hover {
         background-color: red;
     }
 </style>
 
-<form method="get" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data">
     <input type="file" name="file[]" multiple>
     <input type="submit" name="submit" value="Upload image">
 </form>
+
 <?php
 
-    foreach($_FILES['file']['name'] as $key => $name){
+    foreach($_FILES['file']['name'] as $key => $name) {
+
         $tmp_name = $_FILES['file']['tmp_name'][$key];
+
         $dir = 'img';
-        //if (  )
+
         $i++;
+
         $image = explode(".", $name);
+
         move_uploaded_file($tmp_name, "$dir/$i.$image[1]");
-        echo '<div style="width: 100px;">' . '<img style="width: 100%;" src=' . "$dir/$i.$image[1]" . '>' . '</div>';
+
     }
 ?>
 
@@ -79,10 +116,16 @@
 	print_r($files);*/
 ?>
 
+<?php
+
+?>
+
 <div class="test">
 	<?php
+
 		foreach($files as $key => $value){
-			echo "<div><img src=\"img/$value\"><span>+</span></div>";
-		}
+			echo "<div><div class=\"test2\"><img src=\"img/$value\"><span>+</span></div></div>";
+        }
+
 	?>
 </div>
