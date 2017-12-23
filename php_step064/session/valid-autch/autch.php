2e49b9fad077c51efa873ce2c78a3b3a286c6cd3
slavicc064 <slavicc064@gmail.com>
2017-12-23 22:14:43 +0200

    23.12.17 22:14

--- php_step064/session/valid-autch/autch.php
+++ php_step064/session/valid-autch/autch.php
@@ -1,25 +1,50 @@
-<?php //session_start()?>
-<style>
-    form {
-        width: 300px;
-        height: auto;
-        margin: 0 auto;
-    }
-    input {
-        display: block;
-        margin-bottom: 10px;
-        width: 100%;
-        border: 1px solid gainsboro;
-        border-radius: 4px;
-        height: 30px;
-        padding: 0 15px;
-    }
-</style>
+<?php session_start(); ?>
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <title>Название страницы</title>
+    <style>
+		form {
+			background-color: gray;
+			width: 300px;
+			padding: 10px
+		}
+        input {
+            border: 1px white solid;
+            border-radius: 2px;
+            padding: 0 5px;
+			display: block;
+			width: 95%;
+			height: 30px;
+			margin-top: 5px;
+        }
 
-<form action="mod.php" method="post">
-    <input type="text" name="firstName" placeholder="First Name">
-    <input type="text" name="lastName" placeholder="Last Name">
-    <input type="email" name="email" placeholder="example@email.com">
-    <input type="submit" name="ok" value="ok">
-</form>
+        form input.error {
+            border-color: red;
+        }
+        form input.error::placeholder {
+            color: red;
+        }
+    </style>
+</head>
+<body>
 
+	<form action="mod.php" method="post">
+		<input type="text" name="firstName" placeholder="First Name" 
+		value="<?php if ($_SESSION['error_first'] !== 'error') {
+		echo $_SESSION['firstname'];
+		} else if ($_SESSION['error_first'] == 'error') {
+		echo "";
+		} 
+		if (!isset($_SESSION['error_first'])) {
+			echo "";
+		}  ?>" 
+		class="<?php if (isset($_SESSION['error_first'])) {echo $_SESSION['error_first'];} ?>">
+		<input type="text" name="lastName" placeholder="Last Name" value="<?php //echo $_SESSION['lastname']?>" class="<?php if (isset($_SESSION['error_last'])) {echo $_SESSION['error_last'];} ?>">
+		<input type="email" placeholder="E-mail" name="email" class="<?php if (isset($_SESSION['error_email'])) {echo $_SESSION['error_email'];} ?>">
+		<input type="submit" name="ok">
+	</form>
+
+</body>
+</html>
+<?php// session_destroy() ?>
--- php_step064/session/valid-autch/index.php
+++ php_step064/session/valid-autch/index.php
@@ -1,92 +1,12 @@
 <?php
-    /*session_start();
-    if ( isset( $_SESSION['firstname'] ) ) {
+    session_start();
+    if ( $_SESSION['firstname'] == 'first123' && $_SESSION['lastname'] == 'last123' && $_SESSION['email'] == 'email@email.com' ) {
         include 'home.php';
     }
     else {
         include 'autch.php';
-    }*/
+    }
+	echo '<pre>';
+	print_r($_SESSION);
 ?>
 
-<?php session_start(); ?>
-<!doctype html>
-<head>
-    <meta charset="UTF-8">
-    <title>Название страницы</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-
-        .wrapper {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-
-        .form-block {
-            width: 400px;
-            min-height: 200px;
-            background: #99d39f;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 0 20px 0 15px;
-        }
-
-        .form-block form {
-            width: 100%;
-            position: relative;
-        }
-
-        .form-block input {
-            width: 100%;
-            height: 35px;
-        }
-
-        .form-block input[type=submit] {
-            height: auto;
-            margin-top: 15px;
-            margin-bottom: 15px;
-            font-size: 18px;
-            text-transform: uppercase;
-            color: white;
-            background: orangered;
-            border: none;
-            padding: 10px 0 10px 0;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-
-        input[type=text] {
-            border: 1px white solid;
-            border-radius: 5px;
-            padding: 0 5px;
-        }
-
-        form input.error {
-            border-color: red;
-        }
-        form input.error::placeholder {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-
-<div class="wrapper">
-    <div class="form-block">
-        <form action="mod.php" method="post">
-            <input type="text" name="firstName" placeholder="First Name" class="<?php if (isset($_SESSION['error_email'])) {echo $_SESSION['error_email'];} ?>">
-            <input type="text" name="lastName" placeholder="Last Name" class="<?php if (isset($_SESSION['error_email'])) {echo $_SESSION['error_email'];} ?>">
-            <input type="text" placeholder="E-mail" name="email" class="<?php if (isset($_SESSION['error_email'])) {echo $_SESSION['error_email'];} ?>">
-            <input type="submit" value="Подписаться">
-        </form>
-    </div>
-</div>
-
-</body>
-</html>
-<?php session_destroy() ?>
